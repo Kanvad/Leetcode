3fe825d48deb40875d624d9c8f340d1dfde90282
Kanvad <104117240+Kanvad@users.noreply.github.com>
2024-12-07 22:14:11 +0700

    Time: 34 ms (80.00%), Space: 67.3 MB (20.00%) - LeetHub

---
+++ 1760-minimum-limit-of-balls-in-a-bag/1760-minimum-limit-of-balls-in-a-bag.cs
@@ -0,0 +1,28 @@
+public class Solution {
+    // ham kiem tra so lan chia co lon hon gioi han khong
+    private bool CanDivide(int[] nums, int maxBalls, int maxOperations) {
+        int ops = 0;
+        foreach (int n in nums) {
+            ops += (n + maxBalls - 1) / maxBalls - 1;
+            if (ops > maxOperations) return false;
+        }
+        return true;
+    }
+
+    public int MinimumSize(int[] nums, int maxOperations) {
+        // tim kiem nhi phan
+        int left = 1, right = nums.Max();
+        int res = right;
+        
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (CanDivide(nums, mid, maxOperations)) {
+                right = mid;
+                res = right;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return res;
+    }
+}
