5a46620bd5196be734c24301b4300dca534e8ab3
Kanvad <104117240+Kanvad@users.noreply.github.com>
2024-12-17 16:39:33 +0700

    Time: 1 ms (99.81%), Space: 32.5 MB (66.80%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cs
+++ 0009-palindrome-number/0009-palindrome-number.cs
@@ -4,21 +4,32 @@ public class Solution {
         if (x < 0) // neu < 0
             return false;
         
-        string num1 = x.ToString();
-        string num2 = "";
+//         string num1 = x.ToString();
+//         string num2 = "";
         
-        if (num1[0] != num1[num1.Length - 1])
-            return false;
+//         if (num1[0] != num1[num1.Length - 1])
+//             return false;
         
-        for (int i = num1.Length - 1; i >= 0; i--)
-        {
-            num2 += num1[i];
-        }
-        int temp = int.Parse(num2);
+//         for (int i = num1.Length - 1; i >= 0; i--)
+//         {
+//             num2 += num1[i];
+//         }
+//         int temp = int.Parse(num2);
 
-        if (x == temp)
-            return true;
+//         if (x == temp)
+//             return true;
         
-        return false;
+//         return false;
+        
+        int originalNum = x;
+        int reversedNum = 0;
+
+        while (x > 0) {
+            int digit = x % 10;
+            reversedNum = reversedNum * 10 + digit;
+            x /= 10;
+        }
+
+        return originalNum == reversedNum;
     }
 }
