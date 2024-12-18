812b4657c93869978add687b5547e22b9556714d
Kanvad <104117240+Kanvad@users.noreply.github.com>
2024-12-18 18:27:55 +0700

    0412-FIZZ-BUZZ Stats: Time: 8 ms (19.26%), Space: 63.8 MB (7.27%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.cs
+++ 0412-fizz-buzz/0412-fizz-buzz.cs
@@ -1,15 +1,13 @@
 public class Solution {
     public IList<string> FizzBuzz(int n) {
-        List<string> res = new List<string>();
-        
-        for (int i = 1; i <= n; i++)
-        {
-            if (i % 3 == 0 && i % 5 == 0) res.Add("FizzBuzz");
-            else if (i % 3 == 0) res.Add("Fizz");
-            else if (i % 5 ==0) res.Add("Buzz");
-            else res.Add(i.ToString());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) sb.Append("FizzBuzz");
+            else if (i % 3 == 0) sb.Append("Fizz");
+            else if (i % 5 == 0) sb.Append("Buzz");
+            else sb.Append(i);
+            if (i < n) sb.Append(",");
         }
-        
-        return res;
+        return sb.ToString().Split(',');
     }
 }
--- README.md
+++ README.md
@@ -9,4 +9,16 @@ A collection of LeetCode questions to ace the coding interviews! - Created using
 |  |
 | ------- |
 | [0001-two-sum](https://github.com/Kanvad/Leetcode/tree/master/0001-two-sum) |
+## Math
+|  |
+| ------- |
+| [0412-fizz-buzz](https://github.com/Kanvad/Leetcode/tree/master/0412-fizz-buzz) |
+## String
+|  |
+| ------- |
+| [0412-fizz-buzz](https://github.com/Kanvad/Leetcode/tree/master/0412-fizz-buzz) |
+## Simulation
+|  |
+| ------- |
+| [0412-fizz-buzz](https://github.com/Kanvad/Leetcode/tree/master/0412-fizz-buzz) |
 <!---LeetCode Topics End-->
