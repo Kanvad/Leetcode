2d2df5683aba0dfd2d16d556888d0d168f6e5ed6
Kanvad <104117240+Kanvad@users.noreply.github.com>
2024-12-18 18:25:16 +0700

    0001-TWO-SUM Stats: Time: 23 ms (33.17%), Space: 47.8 MB (57.77%) - LeetHub

--- 0001-two-sum/0001-two-sum.cs
+++ 0001-two-sum/0001-two-sum.cs
@@ -1,23 +1,17 @@
 public class Solution {
     public int[] TwoSum(int[] nums, int target) {
-        // Hash table to store number->index mapping
-        Dictionary<int, int> ht = new Dictionary<int, int>();
-        
-        // Iterate through the array
-        for (int i = 0; i < nums.Length; i++) {
-            int tmp = nums[i];
-            
-            // Check if complement (target - current number) exists in hash table
-            if (ht.ContainsKey(target - tmp)) {
-                // If found, return indices of both numbers
-                return new int[] {ht[target - tmp], i};
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j = i + 1; j < nums.Length; j++)
+            {
+                if (nums[j] + nums[i] == target)
+                {
+                    return new int[] { i, j };
+                }
             }
-            
-            // Store current number and its index in hash table
-            ht[tmp] = i;
         }
-        
-        // Return empty array if no solution found
-        return new int[0];
+
+        return new int[] {};
     }
 }
